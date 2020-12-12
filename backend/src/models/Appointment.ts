b0bb539282461aa04196799b6a04bd1a6480e5cf
Erick Pinho <ericklppinho@gmail.com>
2020-12-12 16:24:23 -0300

    added appointments routes; added CreateAppointmentService.ts; added typeorm lib, configured to postgres database; added migrations from appointments and users tables; added foreign key provider_id in appointments table

---
+++ backend/src/models/Appointment.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'provider_id' })
+  providerId: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
+
+export default Appointment;
