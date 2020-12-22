33063861fdf4aa110cddbd4b1509d08ba91be9f8
Erick Pinho <ericklppinho@gmail.com>
2020-12-22 18:49:23 -0300

    updated backend: added cors lib

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import './database';
 
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import uploadConfig from './config/upload';
 import globalErrorHandler from './middlewares/globalErrorHandler';
@@ -10,6 +11,7 @@ import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
