f61b4be0fecf1baac9907be6c721c71e06f326d2
Erick Pinho <ericklppinho@gmail.com>
2020-12-27 00:21:19 -0300

    updated backend

--- backend/src/services/CreateUserService.ts
+++ backend/src/services/CreateUserService.ts
@@ -1,5 +1,5 @@
-import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
+import { getRepository } from 'typeorm';
 
 import AppError from '../errors/AppError';
 import User from '../models/User';
