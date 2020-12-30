45ba5e9904c975c9442ed38e1b58fd28e309409a
Erick Pinho <ericklppinho@gmail.com>
2020-12-29 21:46:20 -0300

    updated mobile: updated SignIn page; added Input and Button component; added react-native-vector-icons library; added Roboto Slab font

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,6 +12,18 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Ok');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
